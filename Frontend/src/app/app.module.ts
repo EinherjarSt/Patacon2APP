ec2c80fccb3b593a9c944a5ee6355fb4b4ab4915
Manuel Gonzalez <mn_gonzalez_16@hotmail.com>
2019-04-09 15:19:23 -0400

    Add truck interface
    Created the first version of the addtruck interface

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AddTruckComponent } from './components/add-truck/add-truck.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddTruckComponent
   ],
   imports: [
     BrowserModule,
