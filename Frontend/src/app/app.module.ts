150dbcf58992442d888a9e2378075b34940656de
Gabriel <gsanhueza15@alumnos.utalca.cl>
2019-04-13 19:02:47 -0400

    Fix collapse button and added logout button

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { ResizeService } from './services/resize.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,11 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    ResizeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
