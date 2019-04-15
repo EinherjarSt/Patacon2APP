6167e7bd2f1e5f8478af46415010c4aedb37e8b3
Benjamin <bpino15@alumnos.utalca.cl>
2019-04-14 23:23:06 -0400

    Added trip filtering by status
    Now list can be filtered by trip status. For instance, if only the "En tránsito" checkbox is selected, then the list will display all trips whose status is "En tránsito".

--- Frontend/src/app/components/trip-list/trip-list.component.ts
+++ Frontend/src/app/components/trip-list/trip-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Trip } from '../../model-classes/trip';
 
 
@@ -9,17 +9,63 @@ import { Trip } from '../../model-classes/trip';
 })
 export class TripListComponent implements OnInit {
 
+  public selectable_status: String[] = ["En tránsito", "Cargando", "Detenido"]
+  
+  //Filtering properties
+  @Input() public selected_status:String[];
+
+
   //Dummy data
   TRIPS: Trip[] = [
-    { id : "0", start_point : "Patacon", end_point :  "Vina", status : "En tránsito hacia viña"},
-    { id : "1", start_point : "Viña", end_point :  "Patacon", status : "En tránsito hacia Patacon"},
-    { id : "2", start_point : "Viña 3", end_point :  "Patacon", status : "Descargando"},
+    {
+      code: "0", status: "En tránsito", shipment: "UVA",
+      departure_point: "patacon", arrival_point: "Viña", date: "03/06/2018",
+      departure_time: "14:20", arrival_time: "17:30"
+    },
+    {
+      code: "0", status: "Cargando", shipment: "UVA",
+      departure_point: "Viña", arrival_point: "patacon", date: "04/06/2018",
+      departure_time: "18:20", arrival_time: "21:30"
+    },
+    {
+      code: "0", status: "En tránsito", shipment: "UVA",
+      departure_point: "Viña 2", arrival_point: "patacon", date: "01/06/2018",
+      departure_time: "14:20", arrival_time: "17:30"
+    }
+
   ];
 
 
-  constructor() { }
+  constructor() {
+    this.selected_status = [];
+   }
 
+   
   ngOnInit() {
+  }
+
+  handleStatusCheckBoxChange(event) {
+    var source_checkbox = event.currentTarget.id;
+    var status;
+
+    console.log("Source element: " + source_checkbox);
+    console.log(this.selected_status);
+    if(source_checkbox === "enTransitoCheckBox") {
+      status = "En tránsito";
+    }
+    else if (source_checkbox === "detenidoCheckBox"){
+      status = "Detenido";
+    }
+    else {
+      status = "Cargando";
+    }
+
+    if (event.target.checked === true) {
+      this.selected_status = this.selected_status.concat(status)
+    }
+    else {
+      this.selected_status = this.selected_status.filter(selected_status => !(selected_status === status));
+    }
   }
 
 }
--- Frontend/src/app/model-classes/trip.ts
+++ Frontend/src/app/model-classes/trip.ts
@@ -1,7 +1,12 @@
 export class Trip {
+    code: String;
     status: String;
-    id : String;
-    start_point: String;
-    end_point  : String;
-    
+    shipment: String; 
+
+    departure_point: String;
+    arrival_point  : String;
+    date: String;
+    departure_time: String;
+    arrival_time: String;
+
   }
