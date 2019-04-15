3619b3bc14248a986568f413123ddb47fd266d99
Feñaña <flopez14@alumnos.utalca.cl>
2019-04-14 21:09:09 -0400

    Add driver interface done

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { ResizeService } from './services/resize.service';
 import { LoginViewComponent } from './components/login-view/login-view.component';
+import { AddDriverComponent } from './components/add-driver/add-driver.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginViewComponent
+    LoginViewComponent,
+    AddDriverComponent
   ],
   imports: [
     BrowserModule,
