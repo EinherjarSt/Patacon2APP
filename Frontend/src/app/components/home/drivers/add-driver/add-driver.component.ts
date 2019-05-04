072a42713fab280f29a83beb17fb887ac2d63a57
Jose <jose.nuez96@gmail.com>
2019-05-04 15:15:38 -0400

    Drivers list/register is complete

--- Frontend/src/app/components/home/drivers/add-driver/add-driver.component.ts
+++ Frontend/src/app/components/home/drivers/add-driver/add-driver.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef} from '@angular/material';
+import { MatDialogRef,MatSnackBar} from '@angular/material';
 import { MAT_DIALOG_DATA } from '@angular/material';
-import { FormControl, Validators} from '@angular/forms';
+import { FormControl,FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { DriversService} from '../../../../services/drivers.service';
 
 
 @Component({
@@ -15,10 +16,28 @@ export class AddDriverComponent implements OnInit {
   email = new FormControl('', [Validators.required, Validators.email]);
 
 
-  constructor(
-    public dialogRef: MatDialogRef<AddDriverComponent>) {}
+  constructor(private snackBar: MatSnackBar,public dialogRef: MatDialogRef<AddDriverComponent>, private driverService : DriversService, 
+    private formBuilder: FormBuilder) {}
 
+  
+  
   ngOnInit() {
+  }
+  
+  registerDriverForm: FormGroup = this.formBuilder.group({
+    run: ['', [Validators.required]],
+    name: ['', [Validators.required]],
+    lastName1: ['', [Validators.required]],
+    lastName2: ['', [Validators.required]],
+    phoneNumber: ['', [Validators.required]],
+  });
+
+  public hasError = (controlName: string, errorName: string) => {
+    return this.registerDriverForm.get(controlName).hasError(errorName);
+  }
+
+  public hasFormError(errorName: string) {
+    return this.registerDriverForm.hasError(errorName);
   }
 
   onCloseConfirm(){
@@ -35,6 +54,26 @@ export class AddDriverComponent implements OnInit {
         this.email.hasError('email') ? 'No es un email válido' :
             '';
   }
+  onFormSubmit() {
+    this.submitData(this.registerDriverForm.value);
+    this.onCloseSubmit();
+    this.openSuccessMessage();
 
+  }
+  submitData(data) {
+    this.driverService.registerDriver(this.registerDriverForm.value).subscribe(
+      response => console.log('Success', response), 
+      error => console.error('Error', error));
+  }
 
+  openSuccessMessage() {
+    this.snackBar.open('El despacho ha sido registrado.', 'Cerrar', {
+      duration: 2000,
+    });
+  }
+
+  onCloseSubmit() {
+    this.dialogRef.close('Confirm');
+
+  }
 }
