17a3eecfd20d09446c36dad61c4b2dda97be5cf0
Gabriel <gsanhueza15@alumnos.utalca.cl>
2019-05-14 01:42:56 -0400

    Add some thing to test with server

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-
+app.get("/", (req, res) => res.json({message: "It is api for patacon"}));
 app.use(require('./authentication'));
 app.use(require('./user'));
 app.use(require('./driver'));
