2e5f026c071de421fd9122e72658f2d7b2346146
Joseshi <jose.nuez96@gmail.com>
2019-06-12 00:02:51 -0400

    link correction (SMS)

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -2,3 +2,7 @@ export const environment = {
   production: true,
   api:'https://www.api-patacon-2.tk'
 };
+export const environmentProd = {
+  production: true,
+  api:'https://www.patacon-2.tk'
+};
